3b8e557ceba3d1aeffa076d2e5b0b8e7a6c7459a
Curtis Updike <cjupdike@icloud.com>
2022-07-31 22:51:34 -0400

    Fetch developer token

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,25 @@
-import { createRoot } from 'react-dom';
+import { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 
 function App() {
-    return <h1>AMP | Apple Music Player</h1>;
+    const [token, setToken] = useState();
+
+    useEffect(() => {
+        fetch('/api/token')
+            .then(data => data.json())
+            .then(({ developerToken }) => {
+                setToken(developerToken);
+            });
+    }, []);
+
+    if (!token) return <h1>Loading...</h1>;
+
+    return (
+        <>
+            <h1>AMP | Apple Music Player</h1>
+            <p>Developer token: {token}</p>
+        </>
+    );
 }
 
 createRoot(document.getElementById('root')).render(<App />);
